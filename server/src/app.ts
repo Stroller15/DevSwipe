234dc7996f2fad45f3421147082e8b695cf26242
Stroller15 <shubhamverma6351@gmail.com>
2024-10-07 23:51:54 +0530

    setup user model

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response, urlencoded } from "express";
+import { User } from "./models/user.model.js";
+import { create } from "domain";
 
 const app: Application = express();
 
 // * default middleware
 app.use(express.json());
+
 
 // * Custom middleware
 
@@ -18,6 +21,38 @@ app.use("/", (err:any, req: any, res: any, next:any) => {
     res.status(500).send("something went wrong");
   }
 })
+
+app.use("/hello/2", (req, res) => {
+  res.send("hellow meoww");
+});
+
+app.use("/hello", (req, res) =>{
+  res.send("hellow hellow")
+})
+
+app.post("/addUser", async (req: Request, res: Response) => {
+  try {
+    const {firstName, lastName, email, password, age, gender } = req.body;
+    const user =  User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      gender
+    })
+
+    res.status(201).json({
+      msg: "user created successfully"
+    })
+  } catch (error) {
+    res.status(400).json({
+      msg: "something went wrong"
+    })
+  }
+})
+
+
 
 
 
