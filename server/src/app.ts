ce6e2e19182f838c73df036e0cebee6db79948fd
Stroller15 <shubhamverma6351@gmail.com>
2024-10-08 23:55:40 +0530

    add signup use functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,46 +11,8 @@ app.use(express.json());
 // * Custom middleware
 
 // * Routes
-app.get("/", (req: Request, res: Response) => {
-  throw new Error("error is coming");
-  res.send("Everything is set don't worry");
-});
-
-app.use("/", (err:any, req: any, res: any, next:any) => {
-  if(err) {
-    res.status(500).send("something went wrong");
-  }
-})
-
-app.use("/hello/2", (req, res) => {
-  res.send("hellow meoww");
-});
-
-app.use("/hello", (req, res) =>{
-  res.send("hellow hellow")
-})
-
-app.post("/addUser", async (req: Request, res: Response) => {
-  try {
-    const {firstName, lastName, email, password, age, gender } = req.body;
-    const user =  User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      age,
-      gender
-    })
-
-    res.status(201).json({
-      msg: "user created successfully"
-    })
-  } catch (error) {
-    res.status(400).json({
-      msg: "something went wrong"
-    })
-  }
-})
+import authRouter from "./routes/auth.route.js"
+app.use("/api/auth", authRouter);
 
 
 
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,24 +1,40 @@
 import {Schema, model} from "mongoose"
-
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true, 
+      trim: true, 
     },
     lastName: {
-        type: String
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String
+      type: String,
+      required: true,
+      unique: true, 
+      match: [/.+\@.+\..+/, "Please enter a valid email"], 
     },
     password: {
-        type: String
-    }, 
+      type: String,
+      required: true,
+      minlength: 6, 
+    },
     age: {
-        type: Number
-    }, 
+      type: Number,
+      min: [0, "Age must be a positive number"], 
+    },
     gender: {
-        type: String
-    }
-})
+      type: String,
+      enum: ["Male", "Female", "Other"], 
+      default: "Other",
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
 
-export const User = model('User', userSchema);
+export const User = model("User", userSchema)
