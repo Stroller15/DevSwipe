b09a955914086e07057e18ee197286a933acc63f
Stroller15 <shubhamverma6351@gmail.com>
2024-10-09 23:40:57 +0530

    implement getuser api

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -31,3 +31,63 @@ export const signUp = async (req: Request, res: Response): Promise<any> => {
       .json({ message: "Server error", error: error.message });
   }
 };
+
+export const login = async (req: Request, res: Response): Promise<any> => {
+  const { firstName, lastName, email, password, age, gender } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already in use" });
+    }
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      gender,
+    });
+    await newUser.save();
+
+    return res
+      .status(201)
+      .json({ message: "User successfully created", user: newUser });
+  } catch (error) {
+    console.error("Error signing up user:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
+
+export const jack = async (req: Request, res: Response): Promise<any> => {
+  const { firstName, lastName, email, password, age, gender } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already in use" });
+    }
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      gender,
+    });
+    await newUser.save();
+
+    return res
+      .status(201)
+      .json({ message: "User successfully created", user: newUser });
+  } catch (error) {
+    console.error("Error signing up user:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
